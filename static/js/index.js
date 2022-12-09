f968958762986db06021c28b90cfcb35ad7e4bcf
Dani Villalba <danivillalba_007@hotmail.com>
2022-12-09 09:53:28 -0300

    ASDFASDF

---
+++ static/js/index.js
@@ -0,0 +1,20 @@
+function changeValue1() {
+    let numero = document.querySelector('.likesValue1');
+    let nuevoNumero = parseInt(numero.innerHTML)
+    nuevoNumero += 1;
+    numero.innerHTML = nuevoNumero;
+}
+
+function changeValue2() {
+    let numero = document.querySelector('.likesValue2');
+    let nuevoNumero = parseInt(numero.innerHTML)
+    nuevoNumero += 1;
+    numero.innerHTML = nuevoNumero;
+}
+
+function changeValue3() {
+    let numero = document.querySelector('.likesValue3');
+    let nuevoNumero = parseInt(numero.innerHTML)
+    nuevoNumero += 1;
+    numero.innerHTML = nuevoNumero;
+}
